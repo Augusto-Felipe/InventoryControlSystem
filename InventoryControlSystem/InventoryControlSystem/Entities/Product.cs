f7fdaddbef69ddbf5a6561e3e68ffb8a55eb58d0
Felipe Augusto <augusto_felipe@icloud.com>
2023-09-12 22:49:28 -0300

    [UPDATE] class product created

---
+++ InventoryControlSystem/InventoryControlSystem/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿namespace InventoryControlSystem.Entities
+{
+    internal class Product
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            Products.Remove(product);
+        }
+
+        public void ListProducts(Product product)
+        {
+
+        }
+    }
+}
--- InventoryControlSystem/InventoryControlSystem/Form1.cs
+++ InventoryControlSystem/InventoryControlSystem/Form1.cs
@@ -1,5 +1,3 @@
-using System.Data;
-
 namespace InventoryControlSystem
 {
     public partial class Form1 : Form
